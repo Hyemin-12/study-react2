5a0326e55e9c25e4aaa8cc07e83f817e5704f1fb
Hyemin-12 <w2112@e-mirim.hs.kr>
2022-09-06 22:29:56 +0900

    벨로퍼트 리액트 1장 11 ~ 12

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, {useRef} from 'react';
 // import Hello from './Hello';
 // import Wrapper from './Wrapper';
 // import Counter from './Counter';
-import InputSample from './InputSample';
+// import InputSample from './InputSample';
+import UserList from './UserList';
 import './App.css';
 
-// 8 ~ 9
+// 12
 function App() {
-  return (
-    <InputSample />
-  );
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    // ...
+
+    nextId.current += 1;
+  };
+  
+  return <UserList users={users} />;
 }
+
+// 11
+// function App() {
+//   return (
+//     <UserList />
+//   );
+// }
+
+// 8 ~ 10
+// function App() {
+//   return (
+//     <InputSample />
+//   );
+// }
 
 // 7
 // function App() {

03deb57cf6c1ee8b571417b6daf605d7eaa12fea
Hyemin-12 <w2112@e-mirim.hs.kr>
2022-10-06 21:39:14 +0900

    useReducer로 구현

--- src/Counter.js
+++ src/Counter.js
@@ -1,20 +1,42 @@
-import React, {useState} from 'react';
+// import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
 
 function Counter() {
-    // 배열의 첫 번째 요소는 현재 상태, 두 번째는 setter 함수, 0은 상태의 기본값(number = 0)
-    const [number, setNumber] = useState(0);
+    // // 배열의 첫 번째 요소는 현재 상태, 두 번째는 setter 함수, 0은 상태의 기본값(number = 0)
+    // const [number, setNumber] = useState(0);
+
+    // const onIncrease = () => {
+    //     // setter 함수를 사용할 때마다 새로운 값을 넣어주는 방법
+    //   setNumber(number + 1);
+    // }
+  
+    // const onDecrease = () => {
+    //     // 함수형 업데이트
+    //     // 기존 값을 업데이트 하는 방법
+    //     setNumber(prevNumber => prevNumber - 1);
+    // }
+  
+    const [number, dispatch] = useReducer(reducer, 0);
 
     const onIncrease = () => {
-        // setter 함수를 사용할 때마다 새로운 값을 넣어주는 방법
-      setNumber(number + 1);
-    }
+      dispatch({ type: 'INCREMENT' });
+    };
   
     const onDecrease = () => {
-        // 함수형 업데이트
-        // 기존 값을 업데이트 하는 방법
-        setNumber(prevNumber => prevNumber - 1);
-    }
-  
+      dispatch({ type: 'DECREMENT' });
+    };  
+
     return (
       <div>
         <h1>{number}</h1>

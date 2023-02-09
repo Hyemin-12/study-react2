570ce442b64dd371dd8a0dd080b47a142b0a8335
hyennin <chm050202@gmail.com>
2023-02-09 19:40:19 +0900

    Card 컴포넌트 만들기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -14,11 +14,12 @@ import LandingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
 import SignUp from './chapter_11/SignUp';
 import Calculator from './chapter_12/Calculator';
+import ProfileCard from './chapter_13/ProfileCard';
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-    <Calculator />
+    <ProfileCard />
 );
 
 // If you want to start measuring performance in your app, pass a function
